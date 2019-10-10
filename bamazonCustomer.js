61cc231b8b52a4102871c0acd89f7561842e0b0a
Augusto <gutoea@gmail.com>
2019-10-10 18:25:02 -0500

    Function buyProduct, connection fixed and added items to table products

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -16,12 +16,27 @@ connection.connect(function(err) {
 });
 
 function display() {
-  connection.query("SELECT * FROM products", function(err, res){
+  connection.query("SELECT * FROM products", function(err, res) {
     if (err) throw err;
     for (var i=0; i < res.length; i++) {
       console.log("Id: " + res[i].id + " | " + "Product: " + res[i].product_name + " | " + "Department: " + res[i].department_name + " | " + "Price: " + res[i].price + " | " + "Quantity: " + res[i].stock_quantity);
-
     };
-      
-}
+  });
+};
+
+function buyProduct() {
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "item",
+      message: "Please enter product ID",
+    },
+    {
+      type: "input",
+      name: "quantity",
+      message: "Please enter quantity",
+    },
+    
+
+  
 }
--- bamazonSchema.sql
+++ bamazonSchema.sql
@@ -12,15 +12,16 @@ price DECIMAL(10,2) NOT NULL,
 stock_quantity INT NOT NULL
 );
 
-SELECT * FROM products
-
 INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("Chubbs Wooden Hand", "Memorabilia", 149.99, 01),
 ("Infinity Gauntlet", "Universal Weapons", 499.99, 03),
 ("Captain Crunch's Wooden Leg", "Memorabilia", 149.99, 02),
 ("Silver Surfers Silver Surfboard" , "Universal Weapons", 299.99, 01),
 ("Water Bottle", "Supplies", 4.99, 97),
-("Salt Shaker", "Home Goods", 1.99, 457);
+("Salt Shaker", "Home Goods", 1.99, 457),
+("Flying Broom", "Witchcraft Supplies", 299.99, 13);
 
-DELETE * FROM products
+SELECT * FROM products
 
+
+/*DELETE * FROM products*/
