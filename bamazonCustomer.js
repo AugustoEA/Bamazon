0118786069d04ff79a12e8477ed97052279db9cb
Augusto <gutoea@gmail.com>
2019-10-10 17:57:28 -0500

    Connection fixed and function display

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,14 +4,8 @@ var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
   host: "localhost",
-
-  // Your port; if not 3306
   port: 3306,
-
-  // Your username
   user: "root",
-
-  // Your password
   password: process.env.DB_PASSWORD,
   database: "bamazon"
 });
@@ -19,5 +13,15 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
-  createProduct();
 });
+
+function display() {
+  connection.query("SELECT * FROM products", function(err, res){
+    if (err) throw err;
+    for (var i=0; i < res.length; i++) {
+      console.log("Id: " + res[i].id + " | " + "Product: " + res[i].product_name + " | " + "Department: " + res[i].department_name + " | " + "Price: " + res[i].price + " | " + "Quantity: " + res[i].stock_quantity);
+
+    };
+      
+}
+}
