7eaa86bb73598cac16cbac3e434d2212b78c92e7
Augusto <gutoea@gmail.com>
2019-10-10 19:48:49 -0500

    Readme with pictures and fixing function display

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Bamazon
+
+## Overview
+
+This is a "Amazon copycat" using SQL as database. The app take orders from customers and as the orders are completed, it reduces the stock total. If an attempt to buy an product out of stock, or purchase more items that are exist in stock at the moment, the order will not be completed and a message will appear showing that is not possible to complete the order.
+
+# Images
+
+![](images/readme1.png)
+
+![](images/readme2.png)
+
+![](images/readme3.png)
+
+![](images/readme4.png)
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var Table = require("cli-table2");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -12,17 +13,36 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
   if (err) throw err;
-  console.log("connected as id " + connection.threadId + "\n");
+  console.log("connected as id " + connection.threadId);
 });
 
-function display() {
-  connection.query("SELECT * FROM products", function(err, res) {
-    if (err) throw err;
-    for (var i=0; i < res.length; i++) {
-      console.log("Id: " + res[i].id + " | " + "Product: " + res[i].product_name + " | " + "Department: " + res[i].department_name + " | " + "Price: " + res[i].price + " | " + "Quantity: " + res[i].stock_quantity);
-    };
+// function display() {
+//   connection.query("SELECT * FROM products", function(err, res) {
+//     if (err) throw err;
+//     for (var i=0; i < res.length; i++) {
+//       console.log("Id: " + res[i].id + " | " + "Product: " + res[i].product_name + " | " + "Department: " + res[i].department_name + " | " + "Price: " + res[i].price + " | " + "Quantity: " + res[i].stock_quantity);
+//     };
+//   });
+// };
+
+var displayItems = function(){
+  var query = "Select * FROM products";
+  connection.query(query, function(err, res){
+    if(err) throw err;
+    var displayTable = new Table ({
+      head: ["Item ID", "Product", "Department", "Price", "Quantity"],
+      colWidths: [10,15,15,10,14]
+    });
+    for(var i = 0; i < res.length; i++){
+      displayTable.push(
+        [res[i].item_id,res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
+        );
+    }
+    console.log(displayTable.toString());
+    purchaseItems();
   });
-};
+}
+displayItems();
 
 function buyProduct() {
   inquirer.prompt([
@@ -54,14 +74,9 @@ function orderItems(item, quantity) {
     console.log(quantity);
     console.log(update.stock_quantity);
     if(err){console.log(err)};
-    
+
     if(quantity <= update.stock_quantity){
       var total = update.price * quantity;
         console.log("Product available!");
         console.log("Total for " + quantity + " " + update.product_name + " is " + total);
       var newQuantity = update.stock_quantity - quantity;
-    }
-    
-  })
-  
-}
